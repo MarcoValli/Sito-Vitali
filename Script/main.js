52a1f8b8f25b28ec9b53e2465c8abd07bddb4fd8
MarcoValli <marco.valli@gmail.com>
2020-04-01 13:39:27 +0200

    Update main.js

--- Script/main.js
+++ Script/main.js
@@ -1,36 +1,14 @@
 $(document).ready(function() {
+    var min = Math.ceil(1);
+    var max = Math.floor(5);
+    var rndCol = Math.floor(Math.random() * (max - min + 1)) + min;
+    const coloriBg = ["#f5997e", "ebcbc8", "#8010c0", "#8010c0", "#155e95", "#a0707d"];
+    const coloriTxt = ["#f5997e", "ebcbc8", "#8010c0", "#8010c0", "#155e95", "#a0707d"];
     
-    const colori = ["#f5997e", "ebcbc8", "#8010c0", "#8010c0", "#155e95", "#a0707d"];
-    const ColoreRandom = colori[Math.floor(Math.random() * colori.length)];
-    $(".sfondo").css("background-color",ColoreRandom);
+    $("body").css("background-color",coloriBg[rndCol]);
+    $(".colTxt").css("background-color",coloriTxt[rndCol]);
+    
 
-
-    var bgcol1 = "#f5997e";
-    var bgcol2 = "#ebcbc8";
-    var bgcol3 = "#8010c0";
-    var bgcol4 = "#155e95";
-    var bgcol5 = "#a0707d";
-
-    function bgColRandomizer() {
-       
-        min = Math.ceil(1);
-        max = Math.floor(5);
-        comb = Math.floor(Math.random() * (max - min + 1)) + min;
-        var bgColor = ("bgcol" + comb);
-        
-        return bgColor};
-    
-    
-     function lancio (colore){
-        
-        $(".sfondo").css("background-color",colore);
-        console.log(colore);
-        
-    }
-    var col = bgColRandomizer();
-       
-    lancio(col);
-    
     })
     
     
